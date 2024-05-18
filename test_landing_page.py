662243ec1c44971a84eb7b16d8927872c36a6ac5
khunsha123 <104314861+khunsha123@users.noreply.github.com>
2024-05-18 17:19:37 +0530

    Add files via upload

---
+++ test_landing_page.py
@@ -0,0 +1,29 @@
+import datetime
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import time
+from utilibox import Toolbox
+
+
+class TestLandingPage(Toolbox):
+    def take_scrn_shot(self):
+        self.driver.save_screenshot(f"screenshots/{datetime.datetime.now()}-image.png")
+
+    def test_landing_page(self):
+        self.url()
+        landing_page_text = self.driver.find_element(By.CSS_SELECTOR, ".brand").text
+        assert landing_page_text == 'Zero Bank', self.take_scrn_shot()
+
+    def test_searchbox(self):
+        search_box=self.driver.find_element(By.CSS_SELECTOR, "#searchTerm")
+        search_box.send_keys('faketest')
+        search_box.send_keys(Keys.ENTER)
+        assert self.driver.find_element(By.CSS_SELECTOR, ".top_offset > h2").text == 'Search Results:'
+        self.home()
+
+    def test_home_page_menu(self):
+        self.driver.find_element(By.CSS_SELECTOR, "#homeMenu").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#onlineBankingMenu").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#feedback").click()
+
+
---
+++ utilibox.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from selenium.webdriver.common.by import By
+
+
+class Toolbox:
+    load_dotenv()
+    secret_username = os.getenv('UNAME')
+    secret_password = os.getenv('PASSWD')
+    invalid_password = os.getenv('INVALID_PASSWD')
+
+    def url(self):
+        self.driver.get(os.getenv('BASE_URL'))
+        self.driver.maximize_window()
+        page_url = self.driver.current_url
+        assert os.getenv('BASE_URL') in page_url
+
+    def login(self, uname, pword):
+        self.driver.find_element(By.CSS_SELECTOR, '#signin_button').click()
+        self.driver.find_element(By.CSS_SELECTOR, "#user_login").send_keys(uname)
+        self.driver.find_element(By.CSS_SELECTOR, '#user_password').send_keys(pword)
+        self.driver.find_element(By.CSS_SELECTOR, '#user_remember_me').click()
+        self.driver.find_element(By.CSS_SELECTOR, 'input[value="Sign in"]').click()
+
+    def home(self):
+        self.driver.back()
+
+    def navigate_to_feedback_page(self):
+        self.driver.find_element(By.CSS_SELECTOR, "#feedback").click()
